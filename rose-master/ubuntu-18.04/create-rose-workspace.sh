da32a56390113b73c43b1fffda5927c1269829e2
Justin Too <justin@doubleotoo.com>
2018-12-20 14:51:46 -0800

    (ROSE) Updates

--- rose-master/ubuntu-18.04/create-rose-workspace.sh
+++ rose-master/ubuntu-18.04/create-rose-workspace.sh
@@ -2,32 +2,38 @@
 
 : ${ROSE_VERSION:=$1}
 : ${ROSE_VERSION:=master}
+: ${ROSE_DESTDIR:=/home/rose/rose}
+: ${ROSE_PREFIX:=${ROSE_DESTDIR}/${ROSE_VERSION}}
+: ${ROSE_WORKSPACE:=${ROSE_PREFIX}/workspace}
 
-source ~/opt/bison/3.0/setup.sh 
-source ~/opt/libtool/2.4/automake/1.14/autoconf/2.69/setup.sh 
-source ~/opt/boost/1.66.0/gcc/4.8.5/setup.sh
+spack load     \
+    boost      \
+    libtool
 
-mkdir -p "~/rose/${ROSE_VERSION}"
-cd "~/rose/${ROSE_VERSION}"
+mkdir -p "${ROSE_WORKSPACE}"
+cd "${ROSE_WORKSPACE}"
  
-cat > setup.sh <<-EOF
-source "${BISON_HOME}/setup.sh"
-source "${LIBTOOL_HOME}/setup.sh"
-source "${BOOST_HOME}/setup.sh"
-export ROSE_WORKSPACE="$(pwd)"
+cat > "${ROSE_PREFIX}/setup.sh" <<-EOF
+export ROSE_PREFIX="${ROSE_PREFIX}"
+export ROSE_WORKSPACE="${ROSE_WORKSPACE}"
 export ROSE_SOURCE="\${ROSE_WORKSPACE}/rose"
 export ROSE_BUILD="\${ROSE_WORKSPACE}/build"
  
-export ROSE_VERSION=${ROSE_VERSION}
-export ROSE_HOME="\${ROSE_WORKSPACE}/installation"
+export ROSE_VERSION="${ROSE_VERSION}"
+export ROSE_HOME="\${ROSE_PREFIX}"
 export PATH="\${ROSE_HOME}/bin:\${PATH}"
 export LD_LIBRARY_PATH="\${ROSE_HOME}/lib:\${LD_LIBRARY_PATH}"
+
+export BOOST_HOME="\$(spack location -i boost)"
+export LD_LIBRARY_PATH="\${BOOST_HOME}/lib:\${LD_LIBRARY_PATH}"
 EOF
  
-source setup.sh
+source "${ROSE_PREFIX}/setup.sh"
 
 mkdir -p "${ROSE_HOME}"
 cp setup.sh "${ROSE_HOME}"
+
+echo 'export "${ROSE_HOME}/setup.sh"' >> /home/rose/.bashrc
  
 git clone https://github.com/rose-compiler/rose-develop.git "${ROSE_SOURCE}"
  
