3c3c59add29397604180a3f207ce8a4c7ac7f814
Justin Too <justin@doubleotoo.com>
2018-12-27 10:22:27 -0800

    (Ubuntu 18.04) Fix dep paths in setup.sh

--- rose-dependencies/ubuntu-18.04/setup.sh
+++ rose-dependencies/ubuntu-18.04/setup.sh
@@ -1,5 +1,5 @@
 source /root/opt/bison/3.0/setup.sh
 source /root/opt/autoconf/2.69/setup.sh
-source /root/opt/automake/1.14/autoconf/2.69/setup.sh
-source /root/opt/libtool/2.4/automake/1.14/autoconf/2.69/setup.sh
-source /root/opt/boost/1.66.0/gcc/4.8.5/setup.sh
+source /root/opt/automake/1.16/autoconf/2.69/setup.sh
+source /root/opt/libtool/2.4/automake/1.16/autoconf/2.69/setup.sh
+source /root/opt/boost/1.69.0/gcc/4.8/setup.sh
