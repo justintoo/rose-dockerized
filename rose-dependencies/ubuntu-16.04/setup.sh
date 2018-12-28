118165aaf48bf04500add8f54135fb2709cd268b
Justin Too <justin@doubleotoo.com>
2018-12-28 15:21:50 -0800

    (Ubuntu 16.04) Manual dependencies

---
+++ rose-dependencies/ubuntu-16.04/setup.sh
@@ -0,0 +1,5 @@
+source /root/opt/bison/3.0/setup.sh
+source /root/opt/autoconf/2.69/setup.sh
+source /root/opt/automake/1.16/autoconf/2.69/setup.sh
+source /root/opt/libtool/2.4/automake/1.16/autoconf/2.69/setup.sh
+source /root/opt/boost/1.69.0/gcc/4.8/setup.sh
