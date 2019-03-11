814e2094075e21528b7ed516980a5c2f572c0673
Justin Too <justin@doubleotoo.com>
2019-03-11 14:07:51 -0700

    (ROSE-1726) Add mkinstaller scripts

---
+++ rose-master/ubuntu-16.04/mkinstaller/setup.sh
@@ -0,0 +1,6 @@
+export MKINSTALLER_HOME="/root/mkinstaller"
+export PATH="${MKINSTALLER_HOME}/bin:${PATH}"
+
+"${MKINSTALLER_HOME}/share/make-boost-dir.sh"
+source "/root/opt/boost/setup.sh"
+
--- rose-master/ubuntu-16.04/mkinstaller/share/mkinstaller-rose.sh
+++ rose-master/ubuntu-16.04/mkinstaller/share/mkinstaller-rose.sh
@@ -1,14 +1,16 @@
 #!/bin/bash -x
 
-: ${SHARE_DIR:="$(cd "$(dirname "$0") && pwd)"}
+: ${SHARE_DIR:="$(cd "$(dirname "$0")" && pwd)"}
+: ${BIN_DIR:="${SHARE_DIR}/../bin"}
 : ${ROSE_VERSION:="$(identityTranslator --version | grep "ROSE (" | sed 's/^ROSE (version: \(.*\))/\1/')"}
 : ${ROSE_FRONTENDS:="+c+cxx"}
 : ${EDG_VERSION:=5.0}
 : ${GCC_VERSION:="$(gcc -dumpversion)"}
 : ${BOOST_VERSION:=1.54.0}
+: ${BOOST_HOME:=/root/opt/boost}
 
-mkinstaller -v2 \
-	-c rose@${ROSE_VERSION}-ubuntu@16.04-gcc@${GCC_VERSION}-boost@${BOOST_VERSION}-edg-@${EDG_VERSION}${ROSE_FRONTENDS} \
+"${BIN_DIR}/mkinstaller" -v2 \
+	-c rose@${ROSE_VERSION}-ubuntu@16.04-gcc@${GCC_VERSION}-boost@${BOOST_VERSION}-edg@${EDG_VERSION}${ROSE_FRONTENDS} \
 	CC=gcc \
 	CXX=g++ \
 	${BOOST_HOME}/{include,lib} \
